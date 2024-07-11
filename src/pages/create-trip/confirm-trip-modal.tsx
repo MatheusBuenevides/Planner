e7395288948aee8a18e99f8dcabd370c008bbbd8
buenevidesMatheus <matheusgomesbuenevides@gmail.com>
2024-07-11 18:55:36 -0300

    Uso da API
    + Adição do Axios
    + Uso da API para as funcionalidades

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -5,11 +5,16 @@ import { Button } from "../../components/button";
 interface ConfirmTripModalProps {
     closeConfirmTripModal: () => void
     createTrip: (event: FormEvent<HTMLFormElement>) => void
+    setOwnerName: (name: string) => void
+    setOwnerEmail: (email: string) => void
+
 }
 
 export function ConfirmTripModal({
     closeConfirmTripModal,
-    createTrip
+    createTrip,
+    setOwnerEmail,
+    setOwnerName
 }: ConfirmTripModalProps) {
     return (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
@@ -29,6 +34,7 @@ export function ConfirmTripModal({
                     <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
                         < User className='text-zinc-400 size-5' />
                         <input
+                            onChange={event => setOwnerName(event.target.value)}
                             name='nome'
                             placeholder="Seu nome completo"
                             className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
@@ -37,6 +43,7 @@ export function ConfirmTripModal({
                     <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
                         < User className='text-zinc-400 size-5' />
                         <input
+                            onChange={event => setOwnerEmail(event.target.value)}
                             type="email"
                             name='email'
                             placeholder="Seu email pessoal"
--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,30 +1,49 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Participants {
+    id: string
+    name: string | null
+    email: string
+    is_confirmed: boolean
+}
+
 
 export function Guests() {
+    const {tripId} = useParams()
+    const [participants, setParticipants] = useState<Participants[] >()
+    
+    useEffect(() => {
+        api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
+    }, [tripId])
+
     return (
         <div className="space-y-6">
             <h2 className="font-semibold text-xl">Convidados</h2>
 
             <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4 ">
-                    <div className="space-y-1.5">
-                        <span className="block fonte-medium">Letícia</span>
-                        <span className="block text-sm text-zinc-400 truncate">
-                            leticia@gmail.com
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
-                <div className="flex items-center justify-between gap-4 ">
-                    <div className="space-y-1.5">
-                        <span className="block fonte-medium">Matheus</span>
-                        <span className="block text-sm text-zinc-400 truncate">
-                            matheus@gmail.com
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
+                {
+                    participants?.map((participant, index) => {
+                        return (
+                            <div key={participant.id} className="flex items-center justify-between gap-4 ">
+                                <div className="space-y-1.5">
+                                    <span className="block fonte-medium">{participant.name ?? `Convidado ${index}`}</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                        {participant.email}
+                                    </span>
+                                </div>
+                                {participant.is_confirmed ? (
+                                    <CheckCircle2 className="text-zinc-400 size-5 shrink-0" />
+
+                                ) : (
+                                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                                )}
+                            </div>
+                    )})
+                }
             </div>
 
             <Button variant="secondary">
